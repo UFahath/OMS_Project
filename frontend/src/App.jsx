b2232a8938bd4a71ead1fbe3c35d68e87cfad36b
aakash <aakash.r@laderatechnology>
2026-02-06 16:07:08 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import LoginSignup from './components/LoginSignup'
 
 const App = () => {
   return (
     <>
     <Navbar/>
-    
+ 
+    <LoginSignup/>
     </>
   )
 }
