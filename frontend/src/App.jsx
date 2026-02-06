2aabd95a3e29ae8fda508bca23c01ecb4d6c45ca
pooja <pooja76212@gmail.com>
2026-02-06 15:03:07 +0530

    Front end context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    
+      <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+
     </>
   )
 }
