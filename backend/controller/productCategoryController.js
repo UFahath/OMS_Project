474b3edf1a6ed36e9432de59b90879bf7a5fce1f
UFahath <u.zfahath49@gmail.com>
2026-02-07 16:22:18 +0530

    Add productCategoryController

---
+++ backend/controller/productCategoryController.js
@@ -0,0 +1,65 @@
+import ProductCategory from "../models/ProductCategory.js";
+
+//@CreateProductCategory
+export const createProductCategory = async (req, res) => {
+  try {
+    const {
+      category_id,
+      category_name,
+      description,
+      parent_category_id = null,
+      status = "ACTIVE",
+    } = req.body;
+
+   
+    if (!category_id || !category_name) {
+      return res.status(400).json({
+        msg: "category_id and category_name are required",
+      });
+    }
+
+
+    const existingCategory = await ProductCategory.findOne({ category_id });
+    if (existingCategory) {
+      return res.status(409).json({
+        msg: "category_id already exists",
+      });
+    }
+
+
+    const newCategory = await ProductCategory.create({
+      category_id,
+      category_name,
+      description,
+      parent_category_id,
+      status,
+    });
+
+    return res.status(201).json({
+      msg: "Product category created successfully",
+      data: newCategory,
+    });
+  } catch (error){
+    if (error.code === 11000) {
+      return res.status(409).json({
+        msg: "Duplicate value found",
+        error: error.keyValue,
+      });
+    }
+
+    return res.status(500).json({
+      msg: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+//@ReadProductCategory
+export const getProductCategory = async(_,res)=>{
+  try{
+   const productCategory = await ProductCategory.find({}).sort({category_name:1});
+   return res.status(200).json({msg:"Product category retrieved",data:productCategory});
+  }catch(err){
+    res.status(500).json({msg:"Internal server error",error:err.message});
+  }
+}
