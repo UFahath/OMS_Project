5565bb00e5e9f34a5bcb0e4b5745dcb3270a62dc
aakash <aakash.r@laderatechnology>
2026-02-07 22:08:22 +0530

    writed productcontroller

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,1 +1,19 @@
-// Post Product
+// Post Product// Post Product
+
+// import ProductCategory from "../model/productCategoryModel"
+import  ProductCategory from "../model/productCategoryModel.js"
+
+
+
+async function getAllProducts(req, res) {
+  try {
+    const products = await ProductCategory.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+
+
+export default getAllProducts
