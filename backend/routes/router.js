7fa60bdfb75c72073996696b29a266807efc345f
pooja <pooja76212@gmail.com>
2026-02-09 12:05:33 +0530

    middleware

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,6 +7,7 @@ import {
 } from "../controller/productCategoryController.js";
 import { createProducts } from "../controller/productController.js";
 import { createSupportTicket } from "../controller/supportTicketController.js";
+import verifyToken from "../middleware/token.js";
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -14,7 +15,7 @@ router.post("/login", login); // http://localhost:5000/api/login
 router.post("/productCategory", createProductCategory);
 router.get("/productCategory", getProductCategory);
 router.post("/addProduct", createProducts);
-router.post("/supportTicket",createSupportTicket); // supportTicket 
+router.post("/supportTicket", verifyToken,createSupportTicket); // supportTicket 
 export default router;
 
 
