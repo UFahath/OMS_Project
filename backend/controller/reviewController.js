f8007515c74fb8a9f24c65fde4d8f297be25eb3b
Ramkumar <ramkumar.r@laderatechnology.com>
2026-02-10 17:23:01 +0530

    reviewController

--- backend/controller/reviewController.js
+++ backend/controller/reviewController.js
@@ -1,4 +1,5 @@
-import { Review } from "../model/review.js";
+import { Review } from '../model/review.js';
+import { OrderHeader } from '../model/orderHeader.js';
 
 export const createReview = async (req, res) => {
   try {
@@ -14,26 +15,34 @@ export const createReview = async (req, res) => {
     ) {
       return res.status(400).json({
         success: false,
-        message: "All required fields must be provided",
+        message: 'All required fields must be provided',
       });
     }
+    const order = await OrderHeader.findById(orderHeaderId);
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: 'Order not found',
+      });
+    }
+
     if (rating < 1 || rating > 5) {
       return res.status(400).json({
         success: false,
-        message: "Rating must be between 1 and 5",
+        message: 'Rating must be between 1 and 5',
       });
     }
     const review = await Review.create({
       customerId,
       productId,
-      orderHeaderId,
+      orderHeaderId : order._id,
       rating,
       reviewComment,
     });
 
     res.status(201).json({
       success: true,
-      message: "Review created successfully.",
+      message: 'Review created successfully.',
       data: review,
     });
   } catch (error) {
@@ -43,13 +52,3 @@ export const createReview = async (req, res) => {
     });
   }
 };
-
-
-
-
-
-
-
-
-
-
