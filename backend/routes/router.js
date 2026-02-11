ecd1e152c4355ee0ad7eae6afb27a1dda1442fad
pooja <pooja76212@gmail.com>
2026-02-11 09:58:00 +0530

    hhhhh

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -37,7 +37,7 @@ router.post('/payment',createPayment); //http://localhost:5000/api/payment
 router.get('/allProducts', getAllProducts)
  router.post('/placeorders',verifyToken,createOrder)
  router.post('/deliveryAddress',createShipment); //http://localhost:5000/api/createShipment
-<<<<<<< Updated upstream
+
 router.get('/supplierOrders', verifyToken, supplierOrders);//
 router.delete('/deleteProduct/:id',deleteSupplierProduct);//http://localhost:5000/api/deleteSupplierProduct
 
