5f0cc4a234eec4aa77f034059f4fc16625961f3e
Ramkumar <ramkumar.r@laderatechnology.com>
2026-02-27 10:08:19 +0530

    update

--- backend/controller/returnController.js
+++ backend/controller/returnController.js
@@ -10,23 +10,6 @@ export const createReturn = async (req, res) => {
         message: 'All required fields must be provided',
       });
     }
-
-    // if (!mongoose.Types.ObjectId.isValid(OrderDetailsId)) {
-    //   return res.status(400).json({
-    //     success: false,
-    //     message: 'Invalid OrderDetailsId format',
-    //   });
-    // }
-
-    // const reason = returnReason.trim();
-    // if (!reason) {
-    //   return res.status(400).json({
-    //     success: false,
-    //     message: 'returnReason cannot be empty',
-    //   });
-    // }
-
-    // Prevent duplicates
     const existing = await Return.findOne({ OrderDetailsId });
     if (existing) {
       return res.status(409).json({
